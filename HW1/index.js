74c9dd1296615392cf47e045b43ed5a2c4e82dc5
RuslanNikitin39 <ruslan-nikitin@mail.ru>
2022-08-20 10:22:26 +0200

    added checks

--- HW1/index.js
+++ HW1/index.js
@@ -1,29 +1,39 @@
 while (true) {
-    
+
     let n = Math.round(Math.random()*1000);
     console.log('Я загадал число:', n);
 
-    let guessNum = prompt('Введите ваше число (от 0 до 999, q - выход):');
-    console.log('Пользователь ввел:', +guessNum);
+    while (true) {
+        
+        let guessNum = prompt('Введите ваше число (от 0 до 999, q - выход):');
+        console.log('Пользователь ввел:', +guessNum);
 
-    if (guessNum === 'q') {
-        alert('Игра окончена!');
-        break; 
-    }
-    if (isNaN(guessNum)) {
-        alert('Вы ввели не число!');
-    } else if (+guessNum >= 1000){
-        alert('Вы ввели число, больше 999!');
-    } else if (+guessNum < 0){
-        alert('Вы ввели число, меньше 0!');
-    } else if (+guessNum === n) {
-        alert('Вы угадали!');
-    } else {
-        if (+guessNum > n) {
+        if (guessNum === 'q') {
+            alert('Игра окончена!');
+            break; 
+        }
+
+        if (isNaN(guessNum)) {
+            alert('Вы ввели не число!');  
+        } else if (+guessNum >= 1000){
+            alert('Вы ввели число, больше 999!');   
+        } else if (+guessNum < 0){
+            alert('Вы ввели число, меньше 0!');  
+        } else if (+guessNum === n) {
+            alert('Вы угадали!');
+            break;
+        } else if (+guessNum > n) {
             alert('Вы не угадали! Ваше число больше.');
-        } else {
+        } else if (+guessNum < n) {
             alert('Вы не угадали! Ваше число меньше.');
         }
-        
+            
+    }
+
+    let guessNum = prompt('Начать новую игру? (y - да, n - нет):');
+
+    if (guessNum === 'n') {
+        alert('Очень жаль. До новых встреч!');
+        break;
     }
 }
